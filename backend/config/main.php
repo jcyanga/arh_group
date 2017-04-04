b5a64812d863628461de53d58198dfc16b78bc1d
jcyanga <jcyanga28@yahoo.com>
2017-04-04 20:20:04 +0800

    My App v2 Second commit

--- backend/config/main.php
+++ backend/config/main.php
@@ -48,9 +48,13 @@ return [
             // 'baseUrl' => 'site',
         // ],
         // 'urlManager' => [
+        //     'class' => 'yii\web\UrlManager',
         //     'enablePrettyUrl' => true,
         //     'showScriptName' => false,
         //     'rules' => [
+        //         '<controller:\w+>/<id:\d+>' => '<controller>/view',
+        //         '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+        //         '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
         //     ],
         // ],
     ],
